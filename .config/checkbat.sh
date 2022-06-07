126277d3b1504d4f48ddc559fc0c2d689b52b1b6
Ananyobrata Pal <ananyo141@gmail.com>
2022-06-07 22:04:07 +0530

    add lf change battery percent checker from 10 secs to 10 mins

--- .config/checkbat.sh
+++ .config/checkbat.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 # daemon script to check for battery
-bat_percent=$(cat /sys/class/power_supply/BAT0/capacity)
 
 while true; do
+	bat_percent=$(cat /sys/class/power_supply/BAT0/capacity)
+
 	if [ "$bat_percent" -lt 15 ]; then
 		DISPLAY=:0 notify-send "Low Battery Warning (${bat_percent}% left)"
 	fi
 
-	sleep 10
+	sleep 600
 done
 
