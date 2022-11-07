295c1b0a70c1df7376f14290f065f339e37e5075
Ananyobrata Pal <ananyo141@gmail.com>
2022-11-08 00:30:18 +0530

    format: add newlines in install script

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
+
 #
 # Use GNU Stow to symlink dotfiles to home directory
 #
 
 cd $(dirname $0)
+
 # symlink everything in root directory, skip .config/
 stow . --ignore=.config -t $HOME -v 2
+
 # symlink everything in .config/ separately to ~/.config
 stow .config -t $HOME/.config -v 2
 
