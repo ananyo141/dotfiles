7a39005cad03850be035104d1813eb556811bf78
Ananyobrata Pal <ananyo141@gmail.com>
2022-11-22 01:31:56 +0530

    fix: check in init.lua file, not a symbolic link

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,14 @@
-/home/ananyo/betaCode/nvim_migrate/init.lua
+-- Clear previous autocommands 
+-- in case of config reload
+vim.api.nvim_clear_autocmds {}
+
+-- Settings
+vim.g.mapleader = " " -- make space leader key
+
+require("user.options")
+require("user.exec_cmd")
+require("user.keybindings")
+require("user.user_commands")
+require("user.autocommands")
+require("user.abbreviations")
+
