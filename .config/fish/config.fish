ecca423888eb7d8e76074bec9e6065f1464eb706
Ananyobrata Pal <ananyo141@gmail.com>
2024-07-03 00:56:04 +0530

    feat(neovim): add configuration switcher between astronvim, lazyvim and kickstart.nvim

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,22 @@ if status is-interactive
     pfetch
 end
 
+function nvims
+  set items "default" "kickstart.nvim" "LazyVim" "NvChad" "AstroNvim"
+  set config (printf "%s\n" $items | fzf --prompt=" Neovim Config >> " --height=~50% --layout=reverse --border --exit-0)
+  
+  if test -z "$config"
+    echo "Nothing selected"
+    return 0
+  end
+  
+  if test "$config" = "default"
+    set config ""
+  end
+  
+  env NVIM_APPNAME=$config nvim $argv
+end
+
 if test -e ~/.aliases
     source ~/.aliases
 end
