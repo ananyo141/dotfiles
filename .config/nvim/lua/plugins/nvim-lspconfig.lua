5ce4b74da2a64bc1ebb83e321e3b15d917e11a99
Ananyobrata Pal <ananyo141@gmail.com>
2024-07-08 14:25:39 +0530

    chore(nvim): use lazydev.nvim instead of deprecated neodev.nvim

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -11,9 +11,9 @@ return {
     -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
     { 'j-hui/fidget.nvim', opts = {} },
 
-    -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
-    -- used for completion, annotations and signatures of Neovim apis
-    { 'folke/neodev.nvim', opts = {} },
+    -- `lazydev.nvim` is a plugin that properly configures LuaLS for editing your Neovim 
+    -- config by lazily updating your workspace libraries.
+    { 'folke/lazydev.nvim', opts = { ft = 'lua' } },
   },
   config = function()
     --  This function gets run when an LSP attaches to a particular buffer.
