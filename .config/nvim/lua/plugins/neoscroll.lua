fce843a13a01baa0885cb18bb53d93645438bda5
Ananyobrata Pal <ananyo141@gmail.com>
2024-07-23 18:45:00 +0530

    feat(nvim): add neoscroll.nvim plugin

---
+++ .config/nvim/lua/plugins/neoscroll.lua
@@ -0,0 +1,16 @@
+return {
+ "karb94/neoscroll.nvim",
+  opts = {
+    mappings = {                 -- Keys to be mapped to their corresponding default scrolling animation
+      '<C-u>', '<C-d>',
+      '<C-b>', '<C-f>',
+      '<C-y>', '<C-e>',
+      'zt', 'zz', 'zb',
+      },
+    hide_cursor = true,          -- Hide cursor while scrolling
+    stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+    respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+    cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+    easing = 'linear',           -- Default easing function
+  },
+}
