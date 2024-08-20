a5ecdad8f18ecfd35be33168dfd443a8161a7e48
Ananyobrata Pal <ananyo141@gmail.com>
2024-08-21 02:56:21 +0530

    feat(nvim): add codesnap, goto preview and incline plugins

---
+++ .config/nvim/lua/plugins/codesnap.lua
@@ -0,0 +1,10 @@
+return { 
+  "mistricky/codesnap.nvim",
+  build = "make",
+  opts = {
+    watermark = "",
+    has_breadcrumbs = true,
+    show_workspace = true,
+    bg_padding = 30,
+  },
+}
---
+++ .config/nvim/lua/plugins/goto-preview.lua
@@ -0,0 +1,8 @@
+return {
+  "rmagatti/goto-preview",
+  event = "BufEnter",
+  config = true, -- necessary as per https://github.com/rmagatti/goto-preview/issues/88
+  opts = {
+    default_mappings = true,
+  }
+}
