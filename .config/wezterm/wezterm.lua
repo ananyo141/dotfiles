7e372cc8f30f7b8ef825f9a782d7956982f76e0f
Ananyobrata Pal <ananyo141@gmail.com>
2024-09-06 02:25:38 +0530

    fix(wezterm): disable beeping

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,8 +5,8 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font_with_fallback({ "Monaspace Radon", "Symbols Nerd Font" })
-
 config.hide_tab_bar_if_only_one_tab = true
+config.audible_bell = "Disabled"
 
 config.window_decorations = "RESIZE"
 config.window_padding = {
@@ -17,9 +17,7 @@ config.window_padding = {
 }
 
 config.font_size = 16
-
 config.color_scheme = "Dracula+"
-
 config.window_background_opacity = 0.8
 
 -- and finally, return the configuration to wezterm
