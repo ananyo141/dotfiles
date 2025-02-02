d5482bae4bcac525dfbc3787cf1c4fe0944f5ac5
Ananyobrata Pal <ananyo141@gmail.com>
2025-02-02 06:10:09 +0530

    chore(nvim): remove redundant telescope fzf dependency

--- .config/nvim/lua/plugins/dropbar.lua
+++ .config/nvim/lua/plugins/dropbar.lua
@@ -1,8 +1,4 @@
 return {
 	"Bekaboo/dropbar.nvim",
 	event = "BufReadPre",
-	-- optional, but required for fuzzy finder support
-	dependencies = {
-		"nvim-telescope/telescope-fzf-native.nvim",
-	},
 }
