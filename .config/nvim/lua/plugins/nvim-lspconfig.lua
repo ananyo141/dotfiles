3615e3b12ee4ef9fb70f1f5381560a00c088845b
Ananyobrata Pal <ananyo141@gmail.com>
2025-07-24 02:51:52 +0530

    fix: change lsp event to bufreadpre

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,7 +2,7 @@ return {
 	-- LSP Configuration & Plugins
 	"neovim/nvim-lspconfig",
 	lazy = true,
-	event = { "BufNewFile", "BufReadPost" },
+	event = { "BufNewFile", "BufReadPre" },
 	dependencies = {
 		-- Automatically install LSPs and related tools to stdpath for Neovim
 		{ "williamboman/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
